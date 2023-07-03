66f766ee5f47f03914faaadb7b20caa76124ea59
Enzo Tonon Morente <enzotmorente@gmail.com>
2023-07-03 13:45:36 -0300

    Update mercadinho.c

--- mercadinho.c
+++ mercadinho.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define ARQUIVO "estoque.txt"
+
 //criação do tipo de dado Produto
 typedef struct {
     int quantidade; //quantiade do produto no estoque
@@ -10,7 +12,7 @@ typedef struct {
 } Produto;
 
 //---------------Prototipos begin
-void inserir_produto(Produto *estoque, int *n);
+void inserir_produto(Produto **estoque, int *n, int total_de_produtos);
 
 void aumentar_estoque(Produto *estoque);
 
@@ -23,18 +25,26 @@ void consultar_estoque(Produto *estoque, int n);
 void consultar_saldo(double saldo);
 
 void aloca(Produto **v, int n);
+
+void ler_arquivo(Produto **estoque, FILE *fp, int *total_de_produtos, int *quantidade_de_produtos_no_estoque_atual, double *saldo);
+
+void salvar(FILE *fp, int quantidade_de_produtos_no_estoque_atual, double saldo, Produto *estoque);
 //-----------------Prototipos end
 
 void aloca(Produto **v, int n){
     *v = (Produto *) malloc(sizeof(Produto) * n); //alocar memória para o número de produtos que pode ter no estoque
 }
 
-void inserir_produto(Produto *estoque, int *n){
+void inserir_produto(Produto **estoque, int *n, int total_de_produtos){
     Produto produto_tmp; //produto auxiliar
 
     scanf(" %s %d %lf", produto_tmp.nome, &produto_tmp.quantidade, &produto_tmp.preco); //ler as informações do produto
 
-    estoque[*n] = produto_tmp; //inserir o nosso produto no estoque no index n 
+    if (*n + 1 >= total_de_produtos){ //se a quantidade de produtos atualmente no estoque for maior do que a quantidade de produtos inicilamente alocada
+        *estoque = realloc(*estoque, *n + 1);
+    }
+
+    (*estoque)[*n] = produto_tmp; //inserir o nosso produto no estoque no index n 
     
     *n +=  1; //aumentar o número de produtos no estoque atual por 1
 }
@@ -93,35 +103,73 @@ void consultar_saldo(double saldo){
     printf("--------------------------------------------------\n");
 }
 
-int main(int argc, char *argv[]){
+void ler_arquivo(Produto **estoque, FILE *fp, int *total_de_produtos, int *quantidade_de_produtos_no_estoque_atual, double *saldo){
+    //ler o arquivo conforme sua organização
+    fread(total_de_produtos, sizeof(int), 1, fp); //ler a primeira parte do arquivo, que será a quantidade de produtos presente no mesmo
+
+    *quantidade_de_produtos_no_estoque_atual = total_de_produtos; //como iremos receber todos os produtos no estoque do arquivo, no fim de sua leitura, a quantidade de produtos contidos no estoque vai ser a quantidade máxima de prodtuos no estoque
+
+    fread(saldo, sizeof(double), 1, fp); //ler a segunda parte do arquivo, quer será o saldo 
+
+    aloca(*estoque, *total_de_produtos); //alocar dinamicamente o número total de produtos que se tem no estoque
+
+    //o resto do arquivo será cada item do vetor estoque de tamanho total_de_produtos
+    for (int i = 0; i < *total_de_produtos; i++){
+        Produto tmp;
+
+        fread(estoque[i], sizeof(Produto), 1, fp); //ler o produto no index atual
+    }
+}
+
+void salvar(FILE *fp, int quantidade_de_produtos_no_estoque_atual, double saldo, Produto *estoque){
+    fclose(fp); //fechar o arquivo, pois ele está em modo de leitura
+
+    fp = fopen(ARQUIVO, "wb"); //abrir o arquivo em modo de escrita
+
+    fwrite(&quantidade_de_produtos_no_estoque_atual, sizeof(int), 1, fp); //escrever a quantidade total de produtos que se tem no estoque, que neste caso, é o número de produtos no estoque
+
+    fwrite(&saldo, sizeof(double), 1, fp); //escrever o salário
+
+    fwrite(estoque, sizeof(Produto), quantidade_de_produtos_no_estoque_atual, fp); //escrever todos os produtos do estoue no arquivo
+}
+
+int main(void){
     FILE *fp; //declarar o ponteiro que irá apontar para o arquivo referente ao estoque
 
     double saldo; //declarar a variavel do saldo
 
+    int total_de_produtos; //declarar a variavel para saber a quantidade de produtos que se tem no total no estoque
+
+    int quantidade_de_produtos_no_estoque_atual; //declarar a variavel referente a quantidade de produtos no estoque no momento atual da execução do programa
+
     fp = fopen("estoque.txt", "rb"); //tentar abrir o arquivo de estoque
+
+    Produto * estoque; //o estoque vai ser um vetor de produtos
 
     if (fp == NULL){ //se o arquivo nao existir
         fclose(fp); //fechar o arquivo que está em modo leitura
 
-        fp = fopen("estoque.txt", "wb"); //abrir (criar) o arquivo em mode de escrita
+        fp = fopen(ARQUIVO, "wb"); //abrir (criar) o arquivo em modo de escrita
+        
+        scanf("%d", total_de_produtos); //ler a quantidade de produtos iformadas pelo usuário já que não se tem um arquivo com as informações necessárias
+
+        scanf("%lf", saldo); //ler o saldo informado pelo usuário já que não se tem um arquivo com as informações necessárias
+
+        aloca(&estoque, total_de_produtos); //alocar dinamicamente o número total de produtos que se tem no estoque
+
+        quantidade_de_produtos_no_estoque_atual = 0; //como nenhum produto foi informado, a quantidade de produtos no estoque atual é 0
+    }else {
+        ler_arquivo(&estoque, fp, &total_de_produtos, &quantidade_de_produtos_no_estoque_atual, &saldo);
     }
-
-    Produto * estoque; //o estoque vai ser um vetor de produtos
-
-    int quantidade_de_produtos_no_estoque_atual = 0;
-
-    //a função atoi passa o argumento que inicialmente é uma string para inteiro
-    aloca(&estoque, atoi(argv[1])); //chamar a função para alocar memória
 
     char comando[2]; //array que vai receber o comando que o programa deve executar
 
     scanf(" %s", comando); //lendo o comando
 
     while (comando[0] != 'F'){ //enquanto o comando não for "FE" o programa deve ficar rodando
-        //switch para saber qual função rodar
-        //ele será divido em duas partes para analisar o comando, formado por duas letras, por completo
+        //dependendo da prmeira letra do comando informado executar a devida função
         switch (comando[0]){
-            case 'I': inserir_produto(estoque, &quantidade_de_produtos_no_estoque_atual);
+            case 'I': inserir_produto(&estoque, &quantidade_de_produtos_no_estoque_atual, total_de_produtos);
                 break;  
             case 'A': aumentar_estoque(estoque);
                 break;
@@ -129,7 +177,7 @@ int main(int argc, char *argv[]){
                 break;
             case 'V': venda(estoque, &saldo);
                 break;
-            case 'C': 
+            case 'C':  //caso a primeira letra do comando seja C, é necessário realizer outro switch, com a segunda letra, para descobrir realmente qual comando devemos executar
                 switch (comando[1]){
                     case 'E': consultar_estoque(estoque, quantidade_de_produtos_no_estoque_atual);
                         break;
@@ -140,5 +188,8 @@ int main(int argc, char *argv[]){
 
         scanf(" %s", comando);
     }
+
+    salvar(fp, quantidade_de_produtos_no_estoque_atual, saldo, estoque);
+
     return 0;
 }
